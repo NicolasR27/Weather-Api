8483527552ad50192ece1e4d87310be0f4c55df8
Nic Rios <nickrios33@yahoo.com>
2024-08-27 21:07:57 -0700

    Initial Commit

---
+++ Weather Api/Weather_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ApiApp.swift
+//  Weather Api
+//
+//  Created by Nicolas Rios on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
